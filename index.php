fdb872bf1187aad6b85315f5c00fdb510a866cc5
1104983 <lukegill12@icloiud.com>
2015-10-19 15:19:04 +0100

    added in index.php

--- index.php
+++ index.php
@@ -6,13 +6,24 @@
 	<body>
         <p>
           <?php
-            echo "Hello World";
-            echo "<br>";
-            echo 5 * 10;
+            echo "Aberdeen Specs, Mugs, and Sausage Rolls";
 
+          $myage = 20;
           $myname = "Jim Feeley";
-          $myage = 15;
-          echo "My name is" . $myname . "and I am " . $myage;
+
+            if ($myage > 21 ){
+            print $myname + "-" + "You are able to buy specs, mugs and sr";
+            }
+
+            else if($myage >18 && $myage <21){
+            print $myname + "-" +"You are able to buy specs, and mugs.";
+            }
+
+            else if ($myage >16 && $myage <18){
+          print $myname + "-" +"You can buy specs";
+          }
+          else {print "You can buy nothing";}
+
 
           ?>
 </p>
