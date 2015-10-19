26eb9657432668f0c3dda610895a4da2d83613bb
1104983 <lukegill12@icloiud.com>
2015-10-19 15:49:51 +0100

    added in index.php

--- index.php
+++ index.php
@@ -9,21 +9,37 @@
             echo "Aberdeen Specs, Mugs, and Sausage Rolls";
             echo "<br> </br>";
 
-          $myage = 20;
-          $myname = "Jim Feeley";
+          //$myage = 20;
+         // $myname = "Jim Feeley";
 
-            if ($myage > 21 ){
-            print $myname . "-" . "You are able to buy specs, mugs and sr";
-            }
+           // if ($myage > 21 ){
+           // print $myname . "-" . "You are able to buy specs, mugs and sr";
+            //}
 
-            else if($myage >18 && $myage <21){
+            /*else if($myage >18 && $myage <21){
             print $myname . "-" ."You are able to buy specs, and mugs.";
             }
 
             else if ($myage >16 && $myage <18){
           print $myname . "-" ."You can buy specs";
           }
-          else {print "You can buy nothing";}
+          else {print "You can buy nothing";} */
+
+          $wantedgoods = "specs";
+
+          switch ($wantedgoods) {
+              case "mugs";
+                  echo "You have to be 18 to buy mugs";
+                  break;
+              case "specs";
+                  echo "You have to be 16 to buy specs";
+                  break;
+              case "sr";
+                  echo "You have to be 21 to buy sausage rolls";
+                  break;
+              default;
+                 echo "You cant buy anything cov";
+          }
 
 
           ?>
