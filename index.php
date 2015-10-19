c0532c82e57fe91dbc1cf595ab6eb64a2b27752f
1104983 <lukegill12@icloiud.com>
2015-10-19 14:54:53 +0100

    added in index.php

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
             echo "Hello World";
             echo "<br>";
             echo 5 * 10;
+
+          $myname = "Jim Feeley";
+          $myage = 15;
+          echo "My name is" . $myname . "and I am " . $myage;
+
           ?>
 </p>
 </body>
