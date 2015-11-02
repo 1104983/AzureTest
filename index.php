b54af3b2853cf5f1e7f56e872c1cbdb630296adc
1104983 <lukegill12@icloiud.com>
2015-11-02 16:23:03 +0000

    added in index.php

--- index.php
+++ index.php
@@ -22,10 +22,11 @@
 <?php
 $username = $_POST["username"];
 $password = $_POST["password"];
+$access_level = "standarduser";
 
 if ($username =="Luke" && $password =="1104983") {
 
-setcookie('access_level','standarduser');
+setcookie('access_level',$access_level);
 setcookie('username',$username);
 
 echo "Hi " . $username . ", login was successful.";
@@ -36,6 +37,19 @@ else if($username == "" && $password == ""){
 else{
     echo "Login Failed for user '".$username. "'";
 }
-  ?>
+
+displayAccessLevelInformation($_COOKIE['access_level']);
+
+
+function displayAccessLevelInformation($accesslevel)
+{
+    if ($accesslevel == "standarduser") {
+        echo "<p>You are currently logged in as a standard user</p>";
+    } elseif ($accesslevel == "root") {
+        echo "<p>You are currently logged in as a root user</p>";
+        echo "<p>You now have access to additional administrative features</p>";
+    }
+}
+?>
 </body>
 </html>
