9fdd35c278f20244e332d7c06e2f43b42219f7ce
1104983 <lukegill12@icloiud.com>
2015-11-02 15:07:25 +0000

    added in index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+  <!DOCTYPE html>
 <html>
 <head lang="en">
     <meta charset="UTF-8">
@@ -7,10 +7,10 @@
 <body>
 <action="display.php" method="post">
 <label>Username: </label>
-<input type="text" name="Username">
+<input type="text" name="username">
 <br>
 <label>Password: </label>
-<input type="password" name="Password">
+<input type="password" name="password">
 <br>
 <input type="submit" value ="submit">
 </form></body>
