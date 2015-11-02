393578d4c894044e0df5e1699445ded1dd029475
1104983 <lukegill12@icloiud.com>
2015-11-02 13:43:14 +0000

    added in index.php

--- index.php
+++ index.php
@@ -1,48 +1,16 @@
-
 <!DOCTYPE html>
 <html>
-    <head>
-	</head>
-	<body>
-        <p>
-          <?php
-            echo "Aberdeen Specs, Mugs, and Sausage Rolls";
-            echo "<br> </br>";
-
-          //$myage = 20;
-         // $myname = "Jim Feeley";
-
-           // if ($myage > 21 ){
-           // print $myname . "-" . "You are able to buy specs, mugs and sr";
-            //}
-
-            /*else if($myage >18 && $myage <21){
-            print $myname . "-" ."You are able to buy specs, and mugs.";
-            }
-
-            else if ($myage >16 && $myage <18){
-          print $myname . "-" ."You can buy specs";
-          }
-          else {print "You can buy nothing";} */
-
-          $wantedgoods = "Jimfeeley";
-
-          switch ($wantedgoods) {
-              case "mugs";
-                  echo "You have to be 18 to buy mugs";
-                  break;
-              case "specs";
-                  echo "You have to be 16 to buy specs";
-                  break;
-              case "sr";
-                  echo "You have to be 21 to buy sausage rolls";
-                  break;
-              default;
-                 echo "You cant buy anything cov";
-          }
-
-
-          ?>
-</p>
-</body>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Deployment Test</title>
+</head>
+<body>
+< action="display.php" method="post">
+<label>Username: </label>
+<input type="text" name="Username">
+<br>
+<label>Password: </label>
+<br>
+<input type="submit" value ="submit">
+</form></body>
 </html>
