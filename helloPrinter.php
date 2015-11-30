1686eadead8850cd506831801ae306a47b4fbe59
1104983 <lukegill12@icloiud.com>
2015-11-30 14:57:22 +0000

    added in index.php

--- helloPrinter.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1104983
- * Date: 26/10/2015
- * Time: 13:11
- */
--- phpmyadmin/libraries/Util.class.php
+++ phpmyadmin/libraries/Util.class.php
@@ -518,7 +518,7 @@ class PMA_Util
         if (defined('TESTSUITE')) {
             /* Provide consistent URL for testsuite */
             return PMA_linkURL('http://docs.phpmyadmin.net/en/latest/' . $url);
-        } else if (file_exists('doc/html/index.html')) {
+        } else if (file_exists('doc/html/login.html')) {
             if (defined('PMA_SETUP')) {
                 return '../doc/html/' . $url;
             } else {
@@ -3389,7 +3389,7 @@ class PMA_Util
             // Values for $cfg['DefaultTabServer']
             switch ($target) {
             case 'welcome':
-                return 'index.php';
+                return 'index2.php';
             case 'databases':
                 return 'server_databases.php';
             case 'status':
